4623382b3f6e8b077681ccb4cfeb5564f96a10db
Pooja_Pandey <poojaam84@gmail.com>
2025-06-13 00:46:26 -0500

    done by Pooja

---
+++ weatherdata.js
@@ -0,0 +1,26 @@
+// weatherData.js
+const weatherDatabase = {
+  "london": {
+    name: "London",
+    temperature: 24,
+    conditions: "Rainy",
+    humidity: 75,
+    wind: "10 km/h"
+  },
+  "newyork": {
+    name: "New York",
+    temperature: 28,
+    conditions: "Sunny",
+    humidity: 60,
+    wind: "8 km/h"
+  },
+  "tokyo": {
+    name: "Tokyo",
+    temperature: 20,
+    conditions: "Cloudy",
+    humidity: 68,
+    wind: "12 km/h"
+  }
+};
+
+module.exports = weatherDatabase;
